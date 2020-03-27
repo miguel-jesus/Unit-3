4fdaeefe2c3a78469c609a355269847c3050c328
miguel-jesus <migueljesuscolom@paucasesnovescifp.cat>
2020-03-27 18:50:15 +0100

    Lesson 3.1
    sin problema fácil de entender

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 15;
+    
+
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
